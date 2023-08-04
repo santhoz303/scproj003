2a3a433d6145dbac0b07da66aae34c80bf901cc9
devsanthoz <devsanthoz@DESKTOP-KMRF5J5.localdomain>
2023-08-04 19:16:12 +0530

    Added new console and classlib

---
+++ ProjCalc.Tests/AdditionTest.cs
@@ -0,0 +1,14 @@
+using ProjCalc;
+namespace ProjCalc.Tests;
+
+public class AdditionTest
+{
+    [Fact]
+    public void AddTwoSingleDigitTest()
+    {
+    //Arrange
+    Calculator MyCalc = new Calculator();
+    //Act
+    //Assert
+    }
+}
